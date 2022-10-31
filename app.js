2859ae4d1214e9654a9212ffed81737bdfd1b396
sajid-ahmed-96 <sajju.0310@gmail.com>
2022-10-31 01:06:37 -0700

    jvassingment

---
+++ app.js
@@ -0,0 +1,148 @@
+alert("Thanks for your input!")
+
+var name = "Mark";
+name = "Ace";
+alert(name);
+
+var nationality;
+nationality = "U.S";
+alert(nationality);
+
+var floogle = "Mark";
+alert(floogle);
+
+var lessonAuthor = "Mark";
+alert(lessonAuthor);
+
+var guyWhoKeepsSayingHisOwnName = "Mark";
+alert(guyWhoKeepsSayingHisOwnName);
+
+var x = "Mark";
+alert(x);
+
+var lastName = "Smith";
+alert(lastName);
+
+var cityOfOrigin = "New Orleans";
+alert(cityOfOrigin);
+
+var aussieGreeting = "g'day";
+alert(aussieGreeting);
+
+var thanx = "Thanks for your input!"
+alert(thanx);
+
+var weight = 150;
+alert(weight + 25);
+
+var originalNum = 23;
+var newNum = originalNum + 7;
+alert(newNum);
+
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+alert(newNum);
+
+var originalNum = 90;
+originalNum = originalNum + 10
+alert(originalNum);
+
+var originalNum = "23";
+var newNum = originalNum + 7;
+alert(newNum);
+
+var originalNum = 23;
+var newNum = originalNum + 7;
+alert(newNum);
+
+// var 1stpresident = "Washington" illegal
+// var prezWhoCame1st = "Washington" legal
+
+alert(144);
+
+var caseQty = 144;
+alert(caseQty);
+
+var popularNumber = 4;
+alert(popularNumber);
+
+var popularNumber = 2 + 2;
+alert(popularNumber);
+
+alert(2 + 2);
+
+var popularNumber = 12 - 24;
+alert(popularNumber);
+
+var popularNumber = 3 * 24;
+alert(popularNumber);
+
+var num = 10;
+var popularNumber = num + 200;
+alert(popularNumber);
+
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+alert(popularNumber);
+
+var whatsLeftOver = 10 % 3;
+alert(whatsLeftOver);
+
+var whatsLeftOver = 9 % 3;
+alert(whatsLeftOver);
+
+var num = 1;
+var newNum = num++;
+alert(newNum);
+
+var num = 1;
+var newNum = ++num;
+alert(newNum);
+
+var num = 1;
+var newNum = num--;
+alert(newNum);
+
+var num = 1;
+var newNum = --num;
+alert(newNum);
+
+var totalCost = 1 + 3 * 4;
+alert(totalCost);
+
+var totalCost = 1 + (3 * 4);
+alert(totalCost);
+
+var totalCost = (1 + 3) * 4;
+alert(totalCost);
+
+var resultOfComputation = (2 * 4) * 4 + 2;
+alert(resultOfComputation);
+
+var resultOfComputation = ((2 * 4) * 4) + 2;
+alert(resultOfComputation);
+
+var resultOfComputation = (2 * 4) * (4 + 2);
+alert(resultOfComputation);
+
+var message = "Thanks for your Input!";
+alert(message);
+
+var userName = "Susan";
+alert("Thanks, " + userName + "!");
+
+var message = "Thanks, ";
+var banger = "!";
+alert(message + userName + banger);
+
+var message = "Thanks, ";
+var userName = "Susan";
+var banger = "!";
+var customMess = message + userName + banger;
+alert(customMess);
+
+alert("2" + "2");
+
+alert("2 plus 2 equals " + 2 + 2);
